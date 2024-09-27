8588a5759dbb6f6fcf9e86b0a5bbcefa9480fda1
santiagosj <sanjs965@gmail.com>
2024-09-26 22:48:39 -0300

    CORS fix

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,20 @@ if (!process.env.JWT_SECRET) {
 }
 
 const app = fastify();
-app.register(fastifyCors, { origin: true });
+app.register(fastifyCors, {
+    origin: (origin, cb) => {
+        const allowedOrigins = ['http://localhost:5173'];
+
+        if (!origin || allowedOrigins.includes(origin)) {
+            // Si origin es undefined (solicitud no desde navegador) o está en la lista permitida, permitir el acceso
+            cb(null, true);
+        } else {
+            // Si no está en la lista permitida, lanzar error de CORS
+            cb(new Error("Not allowed by CORS"), "");
+        }
+    }
+});
+
 
 // Registro de rutas
 authRoutes(app);
